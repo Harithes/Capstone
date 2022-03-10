b491d3a58b69ef7715800dec1aa9e184501c363c
Kiowa Horendeck <44891106+Harithes@users.noreply.github.com>
2022-03-10 08:12:48 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 <?php
     #require 'config.php';
     session_start();
-    $db = 'capstonestudents';
+    $db = 'heroku_ea376caa8695210';
+    //$db = 'capstonestudents';
     $host = 'localhost';
     $dsn = "mysql:host=localhost;dbname=capstonestudents";
     $un = "student";
@@ -18,9 +19,21 @@
     if(!isset($_SESSION['user_login']))
     {
         echo"Please login";
-        echo "<a href=login.php>Login Here</a>";
     }else{
         echo "Welcome " . $_SESSION['fName'] . " " . $_SESSION['lName'] . "!";
         echo "<a href=modules.php>HOME</a> <a href=logout.php>LOGOUT</a> <a href=scoreBoard.php> LEADERBOARD</a>";
     }
+
+    
+    //Get Heroku ClearDB connection information
+    $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
+    $cleardb_db = substr($cleardb_url["path"],1);
+    $active_group = 'default';
+    $query_builder = TRUE;
+    // Connect to DB
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 ?>
