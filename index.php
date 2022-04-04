3df475c446d2677ebac3ca7b7428b97e565991ea
Kiowa Horendeck <44891106+Harithes@users.noreply.github.com>
2022-04-04 13:31:04 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
     <body>
 <?php
     session_start();
-    $db = 'capstonestudents';
-    $host = 'localhost';
-    $dsn = "mysql:host=localhost;dbname=capstonestudents";
-    $un = "student";
-    $pw = "testPW";
+    $db = 'heroku_ea376caa8695210';
+$host = 'us-cdbr-east-05.cleardb.net';
+$dsn = "mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_ea376caa8695210";
+$un = "b36d9559844521";
+$pw = "7383d077";
     try
     {
         $db = new PDO($dsn, $un, $pw);
