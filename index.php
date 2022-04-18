be586638d5601c5316c4f9a90edfe64945054784
Benjamin Stearns <82060500+Kips151@users.noreply.github.com>
2022-04-18 11:03:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,8 @@
     }else if(isset($_SESSION['user_login'])){
         echo "Welcome " . $_SESSION['fName'] . " " . $_SESSION['lName'] . "!<br>";
         echo "<a href=modules.php>HOME</a> <a href=logout.php>LOGOUT</a> <a href=studentGrades.php> GRADES</a><br>";
+    }else if(isset($_SESSION['badPageForIndex'])){
+        echo"Welcome professor " . $_SESSION['lName'] . "!<br>";
     }else if(isset($_SESSION['prof_login'])){
         echo"Welcome professor " . $_SESSION['lName'] . "!<br>";
         echo "<a href=modules.php>HOME</a> <a href=logout.php>LOGOUT</a> <a href=grading.php> STUDENT RESPONSES</a><br>";
